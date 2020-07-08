a6f14d297c80d56b6ab0a8d3a3c71edb648fbc21
Naresh Kumar <kumarnaresh1214@gmail.com>
2020-07-08 21:22:25 +0530

    StstelessWidget Used

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,15 @@ import 'package:flutter/cupertino.dart';
 void main() {
   runApp(MaterialApp(
     title: "Awesome App",
-    home: Container(),
+    home: HomePage(),
   ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepOrangeAccent,
+    );
+  }
 }
