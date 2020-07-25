a107ae6727b3e4e8617e5f44e6dfc997bf918624
Naresh Kumar <kumarnaresh1214@gmail.com>
2020-07-25 09:09:26 +0530

    CH2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(
-        child: Center(child: Text("Hi Abhishek Kumar")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.black,
+                  width: 200,
+                  height: 400,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(10.0),
+                          height: 100,
+                          width: 100,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10.0),
+                          height: 100,
+                          width: 100,
+                          color: Colors.green,
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10.0),
+                          height: 100,
+                          width: 100,
+                          color: Colors.yellow,
+                        ),
+                      ],
+                    ),
+                  ),
+                ))));
   }
 }
