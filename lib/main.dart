5ef771b4c72d967157ee96572f5f2b80c2263a5d
Naresh Kumar <kumarnaresh1214@gmail.com>
2020-07-31 08:38:08 +0530

    Ch4Completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutterdemo/pages/home_pages.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-    theme: ThemeData(primarySwatch: Colors.purple),
-  ));
+  runApp(
+    MaterialApp(
+      title: "Awesome App",
+      home: HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+    ),
+  );
 }
--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutterdemo/ChangeNameCard.dart';
 import 'package:flutterdemo/drawer.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,9 +11,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change me";
+  var url = "https://jsonplaceholder.typicode.com/photos";
+  var data;
   @override
   void initState() {
     super.initState();
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(url);
+    // print(res.body);
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
   }
 
   @override
@@ -23,14 +35,23 @@ class _HomePageState extends State<HomePage> {
         title: Text("Awesome App"),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child:
-                ChangeNmaeCard(myText: myText, nameController: _nameController),
-          ),
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: data != null
+              ? ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: ListTile(
+                          title: Text(data[index]["title"]),
+                          subtitle: Text("ID : ${data[index]["id"]}"),
+                          leading: Image.network(data[index]["url"]),
+                        ));
+                  },
+                  itemCount: data.length,
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
